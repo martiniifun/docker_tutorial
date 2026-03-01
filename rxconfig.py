7cf5c046e4708b40e169e8422e0f60526a925056
ilco <martinii.fun@gmail.com>
2026-03-02 01:18:02 +0900

    update ip addr

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="docker_tutorial",
-    api_url="http://158.180.92.202:8000",
+    api_url="http://168.107.22.109:8000",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
